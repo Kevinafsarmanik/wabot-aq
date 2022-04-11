e896dbe5bae10063e973fafd53018097678fd28a
Vinzzt <97930168+Kevinafsarmanik@users.noreply.github.com>
2022-04-11 21:09:39 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -44,8 +44,8 @@ const defaultMenu = {
 │
 │ Uptime: *%uptime (%muptime)*
 │ Database: %rtotalreg of %totalreg
-│ Github:
-│ %github
+│ 
+│©Kevin afsar
 ╰────
 %readmore`.trimStart(),
   header: '╭─「 %category 」',
