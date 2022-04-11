09f67025f0b719b4b7dc9e5963dc807b98183ac5
Vinzzt <97930168+Kevinafsarmanik@users.noreply.github.com>
2022-04-11 21:08:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 let fs = require('fs')
 let chalk = require('chalk')
 
-global.owner = ['6281515860089', '6281334177340', '6283128734012'] // Put your number here
+global.owner = ['6283131214121', '+6283131214121', '6283131214121'] // Put your number here
 global.mods = [] // Want some help?
-global.prems = [] // Premium user has unlimited limit
+global.prems = [6283131214121] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
@@ -28,7 +28,7 @@ const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
   var sticker_name = 'I hope you\'re fine'
-  var sticker_author = 'Nurutomo'
+  var sticker_author = 'Kepinganteng'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
